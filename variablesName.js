865abe0bd770e223c286642a8716eb0fe3b6212d
MIDHAT-HUSSAIN <135623033+MIDHATHUSSAIN@users.noreply.github.com>
2024-05-23 13:57:01 +0500

    Add files via upload

---
+++ variablesName.js
@@ -0,0 +1,32 @@
+
+// Question 1:
+
+let name , age , education;
+
+// Question 2:
+
+// Legal Variable Names:
+
+// 1: firstName
+// 2: age
+// 3: totalAmount
+// 4: _isValid
+// 5: $quantity
+
+
+// Illegal Variable Names:
+
+// 1: 2ndName (variable names cannot start with a number)
+// 2: first name (variable names cannot contain spaces)
+// 3: user-id (variable names cannot contain hyphens)
+// 4: total#amount (variable names cannot contain special characters other than _ and $)
+// 5: is valid? (variable names cannot contain special characters other than _ and $)
+
+// Question 3:
+
+document.write("<h3>“Rules for naming JS variables.<h3><br>");
+document.write("<p>Variable names can only contain, numbers, $ and _. For example : $my_1stVariable.<p>");
+document.write("<p>Variables must begin with a letter, $ or _. For example : $name, _name or name<p>");
+document.write("<p>Variable names are case sensitive <p>");
+document.write("<p>Variable names should not be JS Keywords.<p>");
+
